ec89f37d048e0c47cd436f33326c77b6d2fa8f13
AsmatZahra-code <asmatzahra195@gmail.com>
2025-06-28 15:57:23 +0500

    fixed footer problem

--- ecommerce-frontend/src/pages/Cart.jsx
+++ ecommerce-frontend/src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from '../components/Navbar/Navbar.jsx'
 import Header from '../components/Header/Header.jsx'
-import Footer from '../components/footer/footer.jsx'
+import Footer from '../components/footer/Footer.jsx'
 import Banner from "../components/ProductDetails/Banner.jsx";
 import MycartMainContent from "../components/Cart/MycartMainContent.jsx";
 
